bc6248eb75a9d91810cfb550143ee5c7f2c2662b
JLGG <hotolete@gmail.com>
2016-05-28 23:17:15 +0200

    Creo clase abstracta FiguraBase y compruebo que no se puede instanciar

---
+++ src/consola/App.java
@@ -0,0 +1,12 @@
+package consola;
+
+import entidadesFigura.FiguraBase;
+
+public class App {
+
+	public static void main(String[] args) {
+		//No se puede instanciar la clase FiguraBase
+		//FiguraBase	miFigura = new FiguraBase();
+	}
+
+}
---
+++ src/entidadesFigura/FiguraBase.java
@@ -0,0 +1,10 @@
+package entidadesFigura;
+
+//abstract no es posible crear objetos de esta clase
+public abstract class FiguraBase {
+	protected int posicionX, posicionY; 
+	
+	public void Dibujar(){
+		
+	}
+}
